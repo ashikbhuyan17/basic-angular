c7cf0f16e87ace36a2ebfe88a17306e07339b8cb
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-02-25 02:48:38 +0600

    basic grow

--- src/app/components/form/form-page/form-page.component.ts
+++ src/app/components/form/form-page/form-page.component.ts
@@ -17,5 +17,48 @@ export class FormPageComponent implements OnInit {
     console.log(data);
 
   }
+  // counter()
+  countValue: number = 0
+  counter(data: string) {
+    // this.countValue++
+    data == "minus" ? this.countValue-- : this.countValue++
+  }
+
+  // interpolation vs property binding :
+  //   interpolation vs property binding :
+  // interpolation string,number .... niye koaj kore bt boolean allow kore na 
+  // bt property binding both allow (boolean,string,num.....)
+  disable: boolean = false
+  bindingData: string = "pretty"
+
+
+
+  // If else condition
+  show = true
+  showValueType = "no"
+
+  //  Switch case
+  color: string = "orange"
+
+
+
+  //  For Loop | *ngFor
+  users = ['ashik', 22, 'swe', '017*****', 'male']
+  userObject = [
+    { name: 'ashik', age: 22, department: 'swe' },
+    { name: 'ashik1', age: 62, department: 'swe' },
+    { name: 'ashik2', age: 12, department: 'swe' },
+    { name: 'ashik3', age: 20, department: 'swe' },
+
+  ]
+
+  // Nested Loop
+  nestedObject = [
+    { name: 'ashik', age: 22, department: 'swe', socialAccount: ['facebook', 'youtube', 'linkdin'] },
+    { name: 'ashik1', age: 62, department: 'swe', socialAccount: ['facebook', 'youtube', 'linkdin'] },
+    { name: 'ashik2', age: 12, department: 'swe', socialAccount: ['facebook', 'youtube', 'linkdin'] },
+    { name: 'ashik3', age: 20, department: 'swe', socialAccount: ['facebook', 'youtube', 'linkdin'] },
+
+  ]
 
 }
